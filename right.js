d6ce80e1951c6afd57d5fa103b0bc05512e03c86
humanitythefuture <humanitythefuture@gmail.com>
2026-01-13 12:13:17 +0530

    edit

--- right.js
+++ right.js
@@ -148,9 +148,9 @@ sph.addEventListener("click",()=>{
 
     const links = document.querySelectorAll("a");
 
-links.forEach(link => {
-  link.setAttribute("target", "_blank");
-  link.setAttribute("rel", "noopener noreferrer");
+links.forEach(e => {
+  e.setAttribute("target", "_blank");
+  e.setAttribute("rel", "noopener noreferrer");
 });
 
 let video = document.getElementById("background-video");
